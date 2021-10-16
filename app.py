0cc9ec1ffd0728b8f7d7e4f11a2ccb16fb2042de
PONCEGH <poncegh@gmail.com>
2021-10-15 21:11:59 -0300

    Atualizada estilização da página
    Inserito modelo inicial página informativa
    
    Inserir modificações banco de dados quinzena 4 e 5

--- app.py
+++ app.py
@@ -27,6 +27,10 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+@app.route('/testePI')
+def teste():
+    return render_template('')
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
