eaf4e5f19b0eeb8cd2d7b2c0d07cd66eabff77a1
Everton_Vieira <2015095@aluno.univesp.br>
2021-10-21 23:48:28 -0300

    Função de seleção

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import sqlite3
+
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+
+
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -9,8 +12,7 @@ def get_db_connection():
 
 def get_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone()
     conn.close()
     if post is None:
         abort(404)
