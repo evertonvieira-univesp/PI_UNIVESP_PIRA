2d9525ef49dcbf2a4c3e490e53bce1b984e509bd
Giancarllo <poncegh@gmail.com>
2022-04-27 01:42:36 -0300

    Modificado o banco de dados para PostgreSQL. Pendências:  - finalização da portabilidade da função create (atualizado 27/04/22 - EM TESTE)  - finalização da portabilidade da tabela resultados;  - finalização da portabilidade da invocação de post por id;  - inclusão do suporte ao mapa (javascript);

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 import psycopg2
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '999888777666'
+app.config['SECRET_KEY'] = '12345678'
 
+engine = create_engine("postgresql://admin:123@localhost:5432/PIUNIVESP2022")
+db = scoped_session(sessionmaker(bind=engine))
+
+app.secret_key = '12345678'#Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
@@ -28,17 +35,15 @@ def get_post(post_id):
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    with conn:
-        with conn.cursor() as curs:
-            curs.execute('SELECT * FROM "resultados"')
-    return render_template('index.html', Resultados=curs)
+    exibe = db.execute ('SELECT * FROM resultados')
+
+    return render_template('index.html', Resultados=exibe)
 
 
 @app.route('/db')
 def db_process():
     conn = get_db_connection()
-    sl_db2 = conn.execute('SELECT * FROM "PIUNIVESP2022.public.posts.contato"').fetchall()
+    sl_db2 = db.execute('SELECT * FROM "PIUNIVESP2022.public.posts.contato"').fetchall()
     conn.close()
     return render_template('db_process.html', contato=sl_db2)
 
@@ -65,3 +70,15 @@ def aprofundando():
 def post(id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        contato = request.form.get('contato')
+        bairro = request.form.get('bairro')
+        db.execute("INSERT INTO posts (contato, bairro) VALUES (:contato, :bairro)" , {"contato": contato, "bairro": bairro})
+        db.commit()
+
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
