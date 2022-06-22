3d326cf5d0d43f4e5f05536065c9e23ddf7ed245
Giancarllo <poncegh@gmail.com>
2022-06-21 22:09:17 -0300

    Atualização banco de dados heroku

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app.config['SECRET_KEY'] = '12345678'
 
 
 #Configuração de conexão PostgreSQL
-engine = create_engine("postgresql://postgres:123456789@univesp.coptxedavsy1.sa-east-1.rds.amazonaws.com:5432/postgres")
-#engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
+engine = create_engine("postgresql://qyaidplwokxgaf:59be526e18d66accd0441832ab9add1fa1c56fdcf4b586ee14a26bb0f30ddaca@ec2-52-22-136-117.compute-1.amazonaws.com:5432/d99ce040bt6uck")
 db = scoped_session(sessionmaker(bind=engine))
 app.secret_key = '123456789'
 app.config["SESSION_PERMANENT"] = True
@@ -21,7 +20,7 @@ def get_post(post_id):
     conn = db.execute()
     with conn:
         with conn.cursor() as curs:
-            curs.execute('SELECT * FROM grupo27.public.posts WHERE post_id = id').fetchone()
+            curs.execute('SELECT * FROM d99ce040bt6uck.public.posts WHERE post_id = id').fetchone()
     conn.close()
     if post is None:
         abort(404)
@@ -29,14 +28,14 @@ def get_post(post_id):
 
 @app.route('/teste')
 def get_geo():
-    geo = db.execute('SELECT row_to_json(row(geo, contato, dt)) FROM grupo27.public.geo').fetchall()
+    geo = db.execute('SELECT row_to_json(row(geo, contato, dt)) FROM d99ce040bt6uck.public.geo').fetchall()
     db.close()
     return json
 
 #Página inicial
 @app.route('/')
 def index():
-    exibe = db.execute('SELECT * FROM grupo27.public.retornos').fetchall()
+    exibe = db.execute('SELECT * FROM d99ce040bt6uck.public.retornos').fetchall()
     geo = db.execute('select json_build_array(geo_lat::numeric,geo_long::numeric,id) from posts_geo').fetchall()
     db.close()
     return render_template('index.html', Res=exibe, Geo_pontos=geo)
@@ -45,7 +44,7 @@ def index():
 #Página tipos de contato
 @app.route('/db')
 def tipo():
-    exibe = db.execute('SELECT * FROM grupo27.public.tipos').fetchall()
+    exibe = db.execute('SELECT * FROM d99ce040bt6uck.public.tipos').fetchall()
     return render_template('db.html', tipos=exibe)
 
 
