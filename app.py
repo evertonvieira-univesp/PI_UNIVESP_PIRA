2eaf4ded27b117160cf4d976f004b25772a433b5
eviei <2015095@aluno.univesp.br>
2022-06-26 20:39:34 -0300

    Removido inserção manual de bairros

--- app.py
+++ app.py
@@ -15,37 +15,12 @@ app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 
 
-#Função para encontrar ID de postagens individuais
-def get_post(post_id):
-    conn = db.execute()
-    with conn:
-        with conn.cursor() as curs:
-            curs.execute('SELECT * FROM d99ce040bt6uck.public.posts WHERE post_id = id').fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
-
-@app.route('/teste')
-def get_geo():
-    geo = db.execute('SELECT row_to_json(row(geo, contato, dt)) FROM d99ce040bt6uck.public.geo').fetchall()
-    db.close()
-    return json
-
 #Página inicial
 @app.route('/')
 def index():
-    exibe = db.execute('SELECT * FROM d99ce040bt6uck.public.retornos').fetchall()
     geo = db.execute('select json_build_array(geo_lat::numeric,geo_long::numeric,id) from posts_geo').fetchall()
     db.close()
-    return render_template('index.html', Res=exibe, Geo_pontos=geo)
-
-
-#Página tipos de contato
-@app.route('/db')
-def tipo():
-    exibe = db.execute('SELECT * FROM d99ce040bt6uck.public.tipos').fetchall()
-    return render_template('db.html', tipos=exibe)
+    return render_template('index.html', Geo_pontos=geo)
 
 
 #Página de informação do grupo
@@ -60,23 +35,14 @@ def aprofundando():
     return render_template('aprofundando.html')
 
 
-#Retorno individual de postagens
-@app.route('/<int:id>')
-def post():
-    post = get_post()
-    return render_template('post.html', post=post)
-
-
 #Criação de postagens
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
         contato = request.form.get('contato')
-        bairro = request.form.get('bairro')
         geo = request.form.get('geo')
-        db.execute("INSERT INTO posts (contato, bairro,geo) VALUES (:contato, :bairro, :geo)" , {"contato": contato, "bairro": bairro, "geo": geo})
+        db.execute("INSERT INTO posts (contato,geo) VALUES (:contato, :geo)" , {"contato": contato, "geo": geo})
         db.commit()
         return redirect(url_for('index'))
     return render_template('create.html')
 
-#geojson
