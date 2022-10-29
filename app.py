dba62934070dc27c14fc9e2157f3eadd25158173
eviei <2015095@aluno.univesp.br>
2022-10-28 22:09:26 -0300

    Manipulação e estilização erros, inclusão opções focos

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345678'
-
 
 #Configuração de conexão PostgreSQL
 engine = create_engine("postgresql://qyaidplwokxgaf:59be526e18d66accd0441832ab9add1fa1c56fdcf4b586ee14a26bb0f30ddaca@ec2-52-22-136-117.compute-1.amazonaws.com:5432/d99ce040bt6uck")
@@ -38,16 +38,19 @@ def aprofundando():
 #Criação de postagens
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    erro = None
     if request.method == 'POST':
         contato = request.form.get('contato')
         geo = request.form.get('geo')
         data = request.form.get('data')
-        if contato == '' or geo =='None':
-            return render_template('create.html')
+        if contato == ''or geo =='None':
+            #return render_template('create.html')
+            erro = 'Erro no preenchimento ou validação da sua localização.'
+
         else:
             db.execute("INSERT INTO posts (contato,geo) VALUES (:contato, :geo)" , {"contato": contato, "geo": geo})
             db.commit()
             return redirect(url_for('index'))
-    return render_template('create.html')
+    return render_template('create.html',erro=erro)
 
-#
+
